fe27025db5ce30f1d82b0701db3a590f4fac79e6
Fabio Malloggi <fabiomalloggi@gmail.com>
2021-12-14 11:14:16 +0100

    Added data handling classes: DataManager and OperationsCSV

---
+++ DietManager/src/main/java/dii/utilities/DataManager.java
@@ -0,0 +1,21 @@
+package dii.utilities;
+
+import java.io.File;
+
+public class DataManager {
+    /***************/
+    private static String fileNameInput = "C:/ProgramData/nutrient.csv";
+    private static String fileNameOutput = "C:/ProgramData/nutrient2.csv";
+    /***************/
+
+    private static OperationsCSV fop = new OperationsCSV();
+
+
+    public static void main(String[] args) {
+        File file1 = new File(fileNameInput);
+        File file2 = new File(fileNameOutput);
+
+        //file2.delete();
+        fop.samplinglinesCSV(file1, file2, 10);
+    }
+}
---
+++ DietManager/src/main/java/dii/utilities/OperationsCSV.java
@@ -0,0 +1,36 @@
+package dii.utilities;
+
+import java.io.*;
+
+public class OperationsCSV {
+
+    public int samplinglinesCSV(File fileInput, File fileOutput, int copyLineEveryNLines){
+
+        fileOutput.delete();
+
+        int readCounter = 0, writeCounter = 0;
+        try(BufferedReader reader = new BufferedReader(new FileReader(fileInput));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileOutput, true)) ){
+
+            String line = reader.readLine();
+            while (line != null) {
+                readCounter++;
+
+                /************* APPLICATION SPECIFIC ***************/
+                /**************************************************/
+                if(readCounter % copyLineEveryNLines == 0){
+                    writer.write(line);
+                    writer.newLine();
+                    writeCounter++;
+                }
+                /**************************************************/
+                line = reader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return writeCounter;
+    }
+}
