c590fc87ecfe4aa1118cbcaab72c0a670a66b2c6
Fabio Malloggi <fabiomalloggi@gmail.com>
2021-12-15 11:33:12 +0100

    add/update methods to reduce Food-Nutrient-Per100g file to only target nutrient

--- DietManager/src/main/java/it/unipi/dii/utilities/DataManager.java
+++ DietManager/src/main/java/it/unipi/dii/utilities/DataManager.java
@@ -1,21 +1,29 @@
 package it.unipi.dii.utilities;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class DataManager {
     /***************/
-    private static String fileNameInput = "C:/ProgramData/nutrient.csv";
-    private static String fileNameOutput = "C:/ProgramData/nutrient2.csv";
+    private static String fileNameInput = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/FoodId-NutrientId-Per100g_2.csv";
+    private static String fileNameOutput = "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/FoodId-NutrientId-Per100g_target.csv";
     /***************/
+    private static final List<String> nutrientIDtargets = Arrays.asList( "1008","1003","1258","1005","2000","1079",
+            "1104","1175","1178","1241","1158","1165",
+            "1087","1090","1101","1091","1095");
+    private static final int nutrientIDFieldInPer100gFile = 2;
 
-    private static OperationsCSV fop = new OperationsCSV();
+    private static OperationsCSV opCSV = new OperationsCSV();
 
+    public static void main(String[] args) throws IOException {
+            File fileInput = new File(fileNameInput);
+            File fileOutput = new File(fileNameOutput);
 
-    public static void main(String[] args) {
-        File file1 = new File(fileNameInput);
-        File file2 = new File(fileNameOutput);
+            opCSV.initialize(fileInput, fileOutput);
+            opCSV.copyFileByLineContainingTargets(nutrientIDtargets, nutrientIDFieldInPer100gFile);
+            opCSV.close();
 
-        //file2.delete();
-        fop.samplinglinesCSV(file1, file2, 10);
-    }
+        }
 }
--- DietManager/src/main/java/it/unipi/dii/utilities/OperationsCSV.java
+++ DietManager/src/main/java/it/unipi/dii/utilities/OperationsCSV.java
@@ -1,8 +1,75 @@
 package it.unipi.dii.utilities;
 
 import java.io.*;
+import java.util.List;
 
 public class OperationsCSV {
+    BufferedReader bufReader;
+    BufferedWriter bufWriter;
+
+    public void initialize(File fileInput, File fileOutput){
+        try{
+            bufReader = new BufferedReader(new FileReader(fileInput));
+            bufWriter = new BufferedWriter(new FileWriter(fileOutput, true));
+        }catch(IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+    public void close(){
+        try{
+            bufReader.close();
+            bufWriter.close();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public int copyFileByLine() throws IOException{
+        int writeCounter = 0;
+
+        String line = bufReader.readLine();
+        while (line != null) {
+
+            bufWriter.write(line);
+            bufWriter.newLine();
+            writeCounter++;
+            line = bufReader.readLine();
+        }
+        return writeCounter;
+    }
+
+    public int copyFileByLineContainingTargets(List<String> targets, int fieldContainingTarget) throws IOException{
+        int writeCounter = 0;
+
+        String line = bufReader.readLine();
+        while (line != null) {
+            if(containTarget(line, targets, fieldContainingTarget)) {
+                bufWriter.write(line);
+                bufWriter.newLine();
+                writeCounter++;
+            }
+            line = bufReader.readLine();
+        }
+
+
+        return writeCounter;
+    }
+
+    public boolean containTarget(String line, List<String> targets, int fieldContainingTarget){
+        String[] tokens = line.split(",");
+        boolean targetFound = false;
+        for(String target: targets){
+            if(tokens[fieldContainingTarget-1].contains(target)) {
+                targetFound = true;
+                break;
+            }
+        }
+        return targetFound;
+    }
+
+
+
+
 
     public int samplinglinesCSV(File fileInput, File fileOutput, int copyLineEveryNLines){
 
@@ -16,14 +83,11 @@ public class OperationsCSV {
             while (line != null) {
                 readCounter++;
 
-                /************* APPLICATION SPECIFIC ***************/
-                /**************************************************/
                 if(readCounter % copyLineEveryNLines == 0){
                     writer.write(line);
                     writer.newLine();
                     writeCounter++;
                 }
-                /**************************************************/
                 line = reader.readLine();
             }
 
