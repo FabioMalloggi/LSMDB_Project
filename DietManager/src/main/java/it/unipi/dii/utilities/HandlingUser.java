7ad037f8bae3699478a4dbf711812705faee5ff9
Tommaso <tommaso.nocchi@gmail.com>
2021-12-17 19:36:17 +0100

    solved the JSON issues for the Users but not the write operation in the corrisponding JSON file

--- DietManager/src/main/java/it/unipi/dii/utilities/HandlingUser.java
+++ DietManager/src/main/java/it/unipi/dii/utilities/HandlingUser.java
@@ -14,27 +14,37 @@ public class HandlingUser {
     public static void generatorUser() throws JSONException{
         File fileOriginalAthlete = new File("./data/derived/athleteR.csv");
         File fileAthleteJOSN = new File("./data/derived/athleteRR.json");
+
         JSONObject collection = new JSONObject();
         OperationsCSV opCSV = new OperationsCSV();
         BufferedWriter bufWriter;
         String[] tokens;
+
         opCSV.initializeR(fileOriginalAthlete);
         JSONArray users = new JSONArray();
+
         try {
             String line = opCSV.bufReader.readLine();
+
             while (line != null) {
                 tokens = line.split(",");
+
                 for(int j = 0; j < tokens.length; j++){
                     tokens[j] = tokens[j].replace("\"", "");
                 }
+
                 String[] names = tokens[1].split(" ");
                 String username = "";
+
                 int i = 0;
                 while(i < names.length){
                     username = username+names[i];
                     i++;
                 }
+
+                //I generate a new user
                 JSONObject user = new JSONObject();
+
                 user.put("_id", tokens[0]);
                 user.put("username", username);
                 user.put("password", "");
@@ -42,6 +52,7 @@ public class HandlingUser {
                 user.put("sex", tokens[2]);
                 user.put("country", tokens[6]);
                 user.put("userType", "standardUser");
+
                 JSONArray eatenFoods = new JSONArray();
                 /*JSONObject eatenFood = new JSONObject();
                 eatenFood.put("eatenFoodID", "");
@@ -54,6 +65,7 @@ public class HandlingUser {
 
 
                 users.put(user);
+                line = opCSV.bufReader.readLine();
             }
             collection.put("users", users);
         }catch(IOException e) {
@@ -61,7 +73,9 @@ public class HandlingUser {
         }
         try{
             bufWriter = new BufferedWriter(new FileWriter(fileAthleteJOSN));
-            bufWriter.write(collection.toString());
+            bufWriter.write("prova");
+            bufWriter.newLine();
+            //bufWriter.write(collection.toString());
         }catch(IOException e) {
             e.printStackTrace();
             System.exit(1);
