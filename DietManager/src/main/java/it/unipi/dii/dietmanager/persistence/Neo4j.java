0e0f8e03d477d34547a77efec5fcc283848a316d
Simone <simonebensixz22@gmail.com>
2021-12-25 01:06:12 +0100

    added methods to remove diet, remove user, look up most followed/popular/succeded diet

--- DietManager/src/main/java/it/unipi/dii/dietmanager/persistence/Neo4j.java
+++ DietManager/src/main/java/it/unipi/dii/dietmanager/persistence/Neo4j.java
@@ -4,13 +4,7 @@ import it.unipi.dii.dietmanager.entities.Diet;
 import it.unipi.dii.dietmanager.entities.Nutritionist;
 import it.unipi.dii.dietmanager.entities.StandardUser;
 import it.unipi.dii.dietmanager.entities.User;
-import org.neo4j.driver.Record;
 import org.neo4j.driver.*;
-import org.neo4j.driver.types.Node;
-import org.neo4j.driver.types.Path;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.neo4j.driver.Values.parameters;
 
@@ -68,19 +62,6 @@ public class Neo4j implements AutoCloseable
     {
         try ( Session session = driver.session() )
         {
-            // I perform the query to understand if this diet already exists
-/*            Integer dietAlreadyExists = session.readTransaction((TransactionWork<Integer>) tx -> {
-                Result result = tx.run( "MATCH (diet: Diet) WHERE id = $id RETURN diet",
-                        parameters( "id", diet.getId()));
-                if(result.hasNext()) {
-                    return 1;
-                }
-                return 0;
-            });
-
-            if(dietAlreadyExists == 1)
-                return false;
-*/
             // I create the Diet node
             session.writeTransaction((TransactionWork<Void>) tx -> {
                 tx.run( "MERGE (:Diet {id: $id, name: $name, followersCount: 0, succededCount: 0, failedCount: 0})",
@@ -174,162 +155,104 @@ public class Neo4j implements AutoCloseable
         return false;
     }
 
-    private void printAge(String name)
-    {
+    public boolean removeDiet(Diet diet){
         try ( Session session = driver.session() )
         {
-            Integer age = session.readTransaction((TransactionWork<Integer>) tx -> {
-                Result result = tx.run( "MATCH (p:Person) WHERE p.name = $name RETURN p.age",
-                        parameters( "name", name) );
-                return result.single().get(0).asInt();
-            });
-            System.out.println(age);
-        }
-    }
-
-    //Find and show all the movie titles an actor acted in
-    private void printActorMovies( final String actorName)
-    {
-        try ( Session session = driver.session() )
-        {
-            List<String> movieTitles = session.readTransaction((TransactionWork<List<String>>) tx -> {
-                Result result = tx.run( "MATCH (p:Person)-[:ACTED_IN]->(m) WHERE p.name = $name" +
-                                " RETURN m.title as Title",
-                        parameters( "name", actorName) );
-                ArrayList<String> movies = new ArrayList<>();
-                while(result.hasNext())
-                {
-                    Record r = result.next();
-                    movies.add(r.get("Title").asString());
-                }
-                return movies;
-            });
-            System.out.println(movieTitles);
-        }
-    }
-
-    //Find and show all the co-actors of an actor
-    private void printCoActors( final String actorName)
-    {
-        try ( Session session = driver.session() )
-        {
-            List<String> coActorsNames = session.readTransaction((TransactionWork<List<String>>) tx -> {
-                Result result = tx.run( "MATCH (p:Person)-[:ACTED_IN]->(m)<-[:ACTED_IN]-(others) " +
-                                "WHERE p.name = $name " +
-                                "RETURN others.name as Coactors",
-                        parameters( "name", actorName) );
-                ArrayList<String> coActors = new ArrayList<>();
-                while(result.hasNext())
-                {
-                    Record r = result.next();
-                    coActors.add(r.get("Coactors").asString());
-                }
-                return coActors;
-            });
-            System.out.println(coActorsNames);
-        }
-    }
-
-    //Find the actor with the maximum number of movies who acted in
-    private void printMostFamousActor()
-    {
-        try ( Session session = driver.session() )
-        {
-            String mostFamousActor = session.readTransaction((TransactionWork<String>) tx -> {
-
-                String query = "MATCH (p:Person)-[:ACTED_IN]->(m:Movie) " +
-                        "RETURN p.name AS Name, count(*) as NumMovies " +
-                        "ORDER BY NumMovies DESC " +
-                        "LIMIT 1";
-                Result result = tx.run( query );
-                return result.single().get("Name").asString();
-            });
-            System.out.println(mostFamousActor);
-        }
-    }
-
-    //Find actors with at least n movies
-    private void printActorsWithAtLeastNMovies(final int n)
-    {
-        try ( Session session = driver.session() )
-        {
-            session.readTransaction((TransactionWork<Void>) tx -> {
-
-                String query = "MATCH (p:Person)-[:ACTED_IN]->(m:Movie) " +
-                        "WITH p.name AS Name, count(*) AS NumMovies " +
-                        "WHERE NumMovies >= $numMovies " +
-                        "RETURN Name, NumMovies";
-                Result result = tx.run(query, parameters("numMovies", n));
-                while (result.hasNext()) {
-                    Record r = result.next();
-                    String name = r.get("Name").asString();
-                    int nMovies = r.get("NumMovies").asInt();
-                    System.out.println(name + " with " + nMovies + " movies");
-                }
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run( "MATCH (diet: Diet{id: $id}) DETACH DELETE diet",
+                        parameters( "id", diet.getId()));
                 return null;
             });
+
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
         }
+        return false;
     }
 
-    //Find all the movies where the average age of actors is lower than the youngest director by 20 years
-    private void printMoviesWithVeryOldDirectors()
-    {
+    public boolean removeUser(User user){
         try ( Session session = driver.session() )
         {
-            session.readTransaction((TransactionWork<Void>) tx -> {
-
-                String query = "MATCH (p:Person)-[:ACTED_IN]->(m:Movie) " +
-                        "WITH m.title AS Title, avg(m.released - p.born) AS AvgActorsAge " +
-                        "MATCH (p2:Person)-[:DIRECTED]->(m2:Movie) WHERE m2.title = Title " +
-                        "WITH m2.title AS Title, min(m2.released - p2.born) AS YoungestDirectorAge, AvgActorsAge " +
-                        "WHERE AvgActorsAge < YoungestDirectorAge - 20 " +
-                        "RETURN Title, YoungestDirectorAge, AvgActorsAge";
-                Result result = tx.run(query);
-                List<Record> records = result.list();
-                for (Record r: records) {
-                    String title = r.get("Title").asString();
-                    int youngestDirectorAge = r.get("YoungestDirectorAge").asInt();
-                    double avgActorsAge = r.get("AvgActorsAge").asDouble();
-                    System.out.println("\"" + title + "\". Youngest director is " + youngestDirectorAge + " years old" +
-                            ", and the average actors' age is " + avgActorsAge);
-                }
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run( "MATCH (user: User{username: $username})-[follows:FOLLOWS]->(diet: Diet)" +
+                                "WITH diet, user, (CASE WHEN follows.status = \"current\" THEN 1 ELSE 0 END) AS followersCountDecrement, " +
+                                "(CASE WHEN follows.status = \"succeded\" THEN 1 ELSE 0 END) AS succededCountDecrement, " +
+                                "(CASE WHEN follows.status = \"failed\" THEN 1 ELSE 0 END) AS failedCountDecrement " +
+                                "SET diet.followersCount = diet.followersCount - followersCountDecrement, " +
+                                "diet.failedCount = diet.failedCount - failedCountDecrement, " +
+                                "diet.succededCount = diet.succededCount - succededCountDecrement DETACH DELETE user",
+                        parameters( "username", user.getUsername()));
                 return null;
             });
+
+            // if the user does not have relationship, then I have to delete him
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run( "MATCH (user: User{username: $username}) DETACH DELETE user",
+                        parameters( "username", user.getUsername()));
+                return null;
+            });
+
+            session.writeTransaction((TransactionWork<Void>) tx -> {
+                tx.run( "MATCH (nutritionist: Nutritionist{username: $username}) DETACH DELETE nutritionist",
+                        parameters( "username", user.getUsername()));
+                return null;
+            });
+
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
         }
+        return false;
     }
 
-    //Find and show the shortest path between two actor which passes through a third actor
-    private void printShortestPathWithIntermediaryActor(final String firstActor, final String lastActor,
-                                                        final String middleActor)
-    {
+    public String lookUpMostFollowedDiet(){
+        String mostFollowedDietID = null;
         try ( Session session = driver.session() )
         {
-            session.readTransaction((TransactionWork<Void>) tx -> {
-
-                String query = "MATCH path = shortestPath((:Person {name: $name1})-[*]-(:Person {name: $name2})) " +
-                        "WHERE ANY(x IN nodes(path) WHERE x.name = $name3) " +
-                        "RETURN path";
-                Result result = tx.run( query,
-                        parameters("name1", firstActor, "name2", lastActor, "name3", middleActor));
-                Path path = result.single().get("path").asPath();
-                String pathString = "--";
-                for(Node n : path.nodes())
-                {
-                    if(n.hasLabel("Person"))
-                    {
-                        pathString += n.get("name").asString() + "--";
-                    }
-                    else if(n.hasLabel("Movie"))
-                    {
-                        pathString += n.get("title").asString() + "--";
-                    }
-                }
-                System.out.println("The length of the path is " + path.length());
-                System.out.println(pathString);
-                return null;
+            mostFollowedDietID = session.readTransaction((TransactionWork<String>) tx -> {
+                Result result = tx.run( "MATCH (diet: Diet) RETURN diet.id AS ID ORDER BY diet.followersCount DESC LIMIT 1");
+                return result.next().get("ID").asString();
             });
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
         }
+        return mostFollowedDietID;
     }
+
+    public String lookUpMostSuccededDiet(){
+        String mostSuccededDietID = null;
+        try ( Session session = driver.session() )
+        {
+            mostSuccededDietID = session.readTransaction((TransactionWork<String>) tx -> {
+                Result result = tx.run( "MATCH (diet: Diet) RETURN diet.id AS ID ORDER BY diet.succededCount DESC LIMIT 1");
+                return result.next().get("ID").asString();
+            });
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return mostSuccededDietID;
+    }
+
+    public String lookUpMostPopularDiet(){
+        String mostPopularDietID = null;
+        try ( Session session = driver.session() )
+        {
+            mostPopularDietID = session.readTransaction((TransactionWork<String>) tx -> {
+                Result result = tx.run( "MATCH (diet: Diet) RETURN diet.id AS ID ORDER BY diet.succededCount+diet.followersCount DESC LIMIT 1");
+                return result.next().get("ID").asString();
+            });
+        }catch(Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return mostPopularDietID;
+    }
+
 
     public static void main( String... args ) throws Exception
     {
@@ -338,46 +261,59 @@ public class Neo4j implements AutoCloseable
             StandardUser user1 = new StandardUser("user1");
             StandardUser user2 = new StandardUser("user2");
             StandardUser user3 = new StandardUser("user3");
+            StandardUser user4 = new StandardUser("user4");
+            StandardUser user5 = new StandardUser("user5");
+            StandardUser user6 = new StandardUser("user6");
+            StandardUser user7 = new StandardUser("user7");
+            StandardUser user8 = new StandardUser("user8");
+            StandardUser user9 = new StandardUser("user9");
+            StandardUser user10 = new StandardUser("user10");
             Nutritionist nutritionist1 = new Nutritionist("nut1");
             Nutritionist nutritionist2 = new Nutritionist("nut2");
-            Diet diet1 = new Diet("diet1", "vegan", nutritionist1);
-            Diet diet2 = new Diet("diet2", "vegan", nutritionist1);
+            Diet diet1 = new Diet("diet1", "vegan1", nutritionist1);
+            Diet diet2 = new Diet("diet2", "vegan2", nutritionist1);
             Diet diet3 = new Diet("diet3", "vegetarian", nutritionist2);
-            System.out.println(neo4j.addUser(user1));
             System.out.println(neo4j.addUser(user1));
             System.out.println(neo4j.addUser(user2));
             System.out.println(neo4j.addUser(user3));
+            System.out.println(neo4j.addUser(user4));
+            System.out.println(neo4j.addUser(user5));
+            System.out.println(neo4j.addUser(user6));
+            System.out.println(neo4j.addUser(user7));
+            System.out.println(neo4j.addUser(user8));
+            System.out.println(neo4j.addUser(user9));
+            System.out.println(neo4j.addUser(user10));
             System.out.println(neo4j.addUser(nutritionist1));
             System.out.println(neo4j.addUser(nutritionist2));
             System.out.println(neo4j.addDiet(diet1));
             System.out.println(neo4j.addDiet(diet2));
             System.out.println(neo4j.addDiet(diet3));
             System.out.println(neo4j.followDiet(user1, diet1));
-            System.out.println(neo4j.followDiet(user2, diet2));
-            System.out.println(neo4j.followDiet(user1, diet1));
+            System.out.println(neo4j.followDiet(user2, diet1));
             System.out.println(neo4j.followDiet(user3, diet1));
+            System.out.println(neo4j.followDiet(user4, diet1));
+            System.out.println(neo4j.followDiet(user5, diet2));
+            System.out.println(neo4j.followDiet(user6, diet2));
+            System.out.println(neo4j.followDiet(user7, diet2));
+            System.out.println(neo4j.followDiet(user8, diet3));
+            System.out.println(neo4j.followDiet(user9, diet3));
+            System.out.println(neo4j.followDiet(user10, diet3));
             System.out.println(neo4j.stopDiet(diet1, user1, true));
-            System.out.println(neo4j.stopDiet(diet1, user1, true));
-            System.out.println(neo4j.stopDiet(diet2, user2, false));
-/*            neo4j.addPerson( "Mario");
-            System.out.println("---------------------------");
-            neo4j.printAge( "Gionatan");
-            System.out.println("===========================");
-
-            neo4j.printActorMovies( "Tom Hanks");
-            System.out.println("---------------------------");
-            neo4j.printCoActors( "Tom Hanks");
-            System.out.println("---------------------------");
-            neo4j.printMostFamousActor();
-            System.out.println("---------------------------");
-            neo4j.printActorsWithAtLeastNMovies(5);
-            System.out.println("---------------------------");
-            neo4j.printMoviesWithVeryOldDirectors();
-            System.out.println("---------------------------");
-            neo4j.printShortestPathWithIntermediaryActor("Tom Hanks",
-                    "Kevin Bacon", "Meg Ryan");*/
+            System.out.println(neo4j.stopDiet(diet1, user2, true));
+            System.out.println(neo4j.stopDiet(diet2, user5, true));
+            System.out.println(neo4j.stopDiet(diet2, user6, true));
+            System.out.println(neo4j.stopDiet(diet2, user7, true));
+/*            System.out.println(neo4j.removeDiet(diet1));
+            System.out.println(neo4j.removeUser(user1));
+            System.out.println(neo4j.removeUser(user4));
+            System.out.println(neo4j.removeUser(nutritionist2));
+*/
+            System.out.println(neo4j.lookUpMostFollowedDiet());
+            System.out.println(neo4j.lookUpMostSuccededDiet());
+            System.out.println(neo4j.lookUpMostPopularDiet());
         }
     }
 }
+
 
 
